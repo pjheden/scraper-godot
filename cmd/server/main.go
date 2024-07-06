4a7f9d27536b2f760ec7774be6f69fc98abaf0f4
simonjanss <simon.jansson1@hotmail.com>
2024-07-06 17:50:02 +0200

    WIP webui

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/pjheden/scraper-godot/views"
+	"github.com/pjheden/scraper-godot/views/components"
 )
 
 func main() {
@@ -18,7 +19,24 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		templ.Handler(views.Index()).ServeHTTP(w, r)
+		assetsTable := components.Table{
+			Columns: []components.TableColumn{
+				{Name: "Title", IsHref: false},
+				{Name: "Creator", IsHref: false},
+				{Name: "Version", IsHref: false},
+				{Name: "Stars", IsHref: false},
+				{Name: "First Commit", IsHref: false},
+				{Name: "Latest Commit", IsHref: false},
+				{Name: "Repository URL", IsHref: true, ConvertValueFunc: func(_ string) string { return "" }},
+			},
+			Rows: [][]string{
+				{"Godot", "Godot Engine", "3.3.3", "2,000", "2021-01-01", "2021-01-01", "https://github.com/pjheden"},
+				{"Godot", "Godot Engine", "3.3.3", "2,000", "2021-01-01", "2021-01-01", "https://github.com/pjheden"},
+				{"Godot", "Godot Engine", "3.3.3", "2,000", "2021-01-01", "2021-01-01", "https://github.com/pjheden"},
+			},
+		}
+
+		templ.Handler(views.Index(assetsTable)).ServeHTTP(w, r)
 	})
 
 	fmt.Println("Listening to 8081...")
